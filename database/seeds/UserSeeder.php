368b05045737477fc9449c62abbae0ac55f6ca52
anish376 <shrestha.anish001@gmail.com>
2021-03-31 17:56:18 +0545

    user seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('secret'),
+        ]);
+    }
+}
